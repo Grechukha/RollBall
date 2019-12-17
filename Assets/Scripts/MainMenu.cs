1d82b8bc94dc79ecb194c37db2a85f37517eb045
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2019-12-17 18:50:01 +0200

    Добавил сцену "Главное меню"

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Animator _animatorAuthorsPanel;
+
+    public void Play(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void AuthorsPanelSet()
+    {
+        _animatorAuthorsPanel.SetBool("isAuthorsPanelDisplayed", true);
+    }
+
+    public void AuthorsPanelReset()
+    {
+        _animatorAuthorsPanel.SetBool("isAuthorsPanelDisplayed", false);
+    }
+    public void Exid()
+    {
+        Application.Quit();
+    }
+}
