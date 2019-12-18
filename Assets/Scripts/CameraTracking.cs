989c7c75bf7708ac3df867fe86ec1005c4caf771
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2019-12-18 17:21:46 +0200

    Добавлен скрипт передвижения камеры

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    private Camera _camera;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+        transform.position = new Vector3(0, 0, -10);       
+    }
+
+    private void Update()
+    {
+        СalculateOffset();
+        transform.position = new Vector3(_player.transform.position.x, transform.position.y, transform.position.z) + _offset;
+    }
+
+    private void СalculateOffset()
+    {
+        _offset = new Vector3(_camera.orthographicSize * _camera.aspect * 0.66f, 0, 0);
+    }
+}
