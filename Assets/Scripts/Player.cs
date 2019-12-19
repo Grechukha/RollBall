73c27214c7f09385771a12464b9ab57e5331f7d0
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2019-12-19 14:33:50 +0200

    Исправлен прыжок Теперь можно прыгать на разную высоту

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,30 +3,42 @@
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speade;
-    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _jumpForce = 0.25f;
+    [SerializeField] private float _maxJumpTime = 0.25f;
+    private float _storeMaxJumpTime;
     private Rigidbody2D _rigidbody2D;
     private bool _isGrounded;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        _storeMaxJumpTime = _maxJumpTime;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-
+        Jump();
         Roll();
     }
 
     private void Jump()
     {
-        if (_isGrounded)
+        if (_isGrounded && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            _rigidbody2D.AddForce(new Vector2(0, _jumpForce));
+            _maxJumpTime = _storeMaxJumpTime;
+        }
+
+        if (_maxJumpTime > 0 && Input.GetKey(KeyCode.Mouse0))
+        {
+            _maxJumpTime -= Time.deltaTime;
+
+            _rigidbody2D.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0) && !_isGrounded)
+        {
+            _maxJumpTime = -1;
         }
     }
 
