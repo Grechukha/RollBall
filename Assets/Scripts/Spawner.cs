e85b75b33f5a83f9045e1858ed83010c9644d340
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2019-12-19 13:39:59 +0200

    Добавлен спавнер объектов

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private float _minDelay;
+    [SerializeField] private float _maxDelay;
+    [SerializeField] private float _firstSpawn;
+    private float _nextSpawn;
+
+    private void Start()
+    {
+        _nextSpawn = _firstSpawn;
+    }
+
+    private void Update()
+    {
+        if (Time.time > _nextSpawn)
+        {
+            float xPosition = transform.position.x;
+            float yPosition = Random.Range(-transform.localScale.y / 2, transform.localScale.y / 2) + transform.position.y;
+            float zPosition = transform.position.z;
+
+            Instantiate(_gameObject, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
+            _nextSpawn = Time.time + Random.Range(_minDelay, _maxDelay);
+        }
+    }
+}
