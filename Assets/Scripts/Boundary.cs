b53743a99667436cd790ed7bbad3ed6cafd889ad
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2019-12-19 21:46:32 +0200

    Добавлено удаление объектов со сцены

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var isRemovableObject = !collision.gameObject.GetComponent<GroundPlatform>() && !collision.gameObject.GetComponent<Player>();
+
+        if (isRemovableObject)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+}
