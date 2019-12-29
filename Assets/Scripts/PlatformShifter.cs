43b3a1765aa52a2fab6bfa665ca3ec25387d7194
Vladislav Grechukha <grechukha.vladislav@yahoo.com>
2019-12-29 19:17:23 +0200

    Исправил имя переменной

--- Assets/Scripts/PlatformShifter.cs
+++ Assets/Scripts/PlatformShifter.cs
@@ -6,7 +6,7 @@ public class PlatformShifter : MonoBehaviour
 {
     [SerializeField] private PlayerMovement _player;
 
-    private Queue<GroundPlatform> _gndPlatforms = new Queue<GroundPlatform>();
+    private Queue<GroundPlatform> _groundPlatforms = new Queue<GroundPlatform>();
     private float _nextShiftPositionX;
 
     private void Start()
@@ -16,7 +16,7 @@ public class PlatformShifter : MonoBehaviour
 
         foreach (var platform in platforms)
         {
-            _gndPlatforms.Enqueue(platform);
+            _groundPlatforms.Enqueue(platform);
         }
     }
 
@@ -30,13 +30,13 @@ public class PlatformShifter : MonoBehaviour
 
     private void ShiftPlatform()
     {
-        var firstPlatform = _gndPlatforms.Dequeue();
+        var firstPlatform = _groundPlatforms.Dequeue();
 
-        Vector3 newPosition = _gndPlatforms.ToArray()[_gndPlatforms.Count - 1].EndPoint.position - firstPlatform.StartPoint.localPosition;
+        Vector3 newPosition = _groundPlatforms.ToArray()[_groundPlatforms.Count - 1].EndPoint.position - firstPlatform.StartPoint.localPosition;
         firstPlatform.transform.position = newPosition;
 
-        _gndPlatforms.Enqueue(firstPlatform);
+        _groundPlatforms.Enqueue(firstPlatform);
 
-        _nextShiftPositionX = _gndPlatforms.ToArray()[1].transform.position.x;
+        _nextShiftPositionX = _groundPlatforms.ToArray()[1].transform.position.x;
     }
 }
